bca684e614ba606214fff98f58435a9b65bf45a0
Marguerite El Mbimbey <deido.elmbimbey@edu-its.it>
2026-09-01 23:52:39 +0200

    Refactor App component to include LanguageProvider
    Wrap the main application in a LanguageProvider and add CodeRain component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
+import { LanguageProvider } from "./i18n";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Timeline from "./components/Timeline";
 import Contact from "./components/Contact";
+import CodeRain from "./components/CodeRain";
 
 function App() {
   return (
-    <div className="bg-ink text-white min-h-screen">
-      <Nav />
-      <Hero />
-      <About />
-      <Projects />
-      <Timeline />
-      <Contact />
-    </div>
+    <LanguageProvider>
+      <div className="text-white min-h-screen relative">
+        <CodeRain />
+        <div className="relative z-10">
+          <Nav />
+          <Hero />
+          <About />
+          <Projects />
+          <Timeline />
+          <Contact />
+        </div>
+      </div>
+    </LanguageProvider>
   );
 }
+
+export default App;
+
 
 export default App;
